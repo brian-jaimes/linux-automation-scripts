c69e3c6558cc50bbd6fcb60c2bf8e8faa3e4255e
Brian Jaimes <brianjaimes@Mac.lan>
2026-03-17 21:16:33 -0500

    first automation script

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Starting automation script..."
+
+mkdir test_folder
+touch test_folder/file1.txt
+touch test_folder/file2.txt
+
+echo "Files created successfully"
+
+ls test_folder
+
